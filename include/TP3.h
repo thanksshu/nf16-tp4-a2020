57b34d5698e6eff8fa1b7178a6434ee67f9d9ab5
thanksshu <hezihanshangyuan@gmail.com>
2020-11-24 23:34:23 +0100

    add CMakeLists.txt

--- include/TP3.h
+++ include/TP3.h
@@ -4,44 +4,48 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct t_position {
+typedef struct t_position
+{
     int numero_ligne;
     int ordre;
     int numero_phrase;
     struct t_position *suivant;
 } t_Position;
 
-typedef struct t_listesPosition {
+typedef struct t_listesPosition
+{
     t_Position *debut;
     int nb_elements;
     struct t_listesPosition *suivant;
 } t_ListesPosition;
 
-typedef struct t_noeud {
-    char* mot;
+typedef struct t_noeud
+{
+    char *mot;
     int nb_occurences;
-    t_ListesPosition* positions;
-    t_Noeud* filsGauche;
-    t_Noeud* filsDroit;
+    t_ListesPosition *positions;
+    t_Noeud *filsGauche;
+    t_Noeud *filsDroit;
 } t_Noeud;
 
-typedef struct t_index {
-    t_Noeud* racine;
+typedef struct t_index
+{
+    t_Noeud *racine;
     int nb_mots_differents;
     int nb_mots_total;
 } t_Index;
 
 //cette fonction créée une liste de position vide
-t_ListePositions* creer_liste_positions();
+t_ListePositions *creer_liste_positions();
 
 //cette fonction ajoute un nouvel element dans une liste de position triée
 int ajouter_position(t_ListePositions *listeP, int ligne, int ordre, int num_phrase);
 
 //cette fonction créée un index vide
-t_Index* creer_index();
+t_Index *creer_index();
 
 //cette fonction recherche un mot dans un index
-t_Noeud* rechercher_mot(t_Index *index, char *mot);
+t_Noeud *rechercher_mot(t_Index *index, char *mot);
 
 //cette fonction permet d'ajouter un noeud dans l'index
 int ajouter_noeud(t_Index *index, t_Noeud *noeud);
